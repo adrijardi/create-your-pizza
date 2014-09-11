40c6d52b92b39526692e661f84c43e41d4f14297
Adrian Lopez <adrijardi@gmail.com>
2014-09-11 12:37:42 +0100

    Add more bases and toppings

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,13 @@
-insert into Pizza_Base (id, name, description, active, price) VALUES (1, 'Thin', 'Thin and crispy base', 1, 3.75);
-insert into Pizza_Base (id, name, description, active, price) VALUES (2, 'Big dough', 'More dough', 1, 5.75);
+insert into Pizza_Base (id, name, description, active, price) VALUES (1, 'Thin and small', 'Thin and crispy base', 1, 3.75);
+insert into Pizza_Base (id, name, description, active, price) VALUES (2, 'Thin and wide', 'Your preferred crust but bigger', 1, 4.95);
+insert into Pizza_Base (id, name, description, active, price) VALUES (3, 'Fully loaded', 'More dough, bigger, BETTER!', 1, 8.50);
 
 insert into Topping (id, name, description, active, price) VALUES (1, 'Mozzarella', 'Mozzarella di buffala, it''s great!', 1, 1.50);
 insert into Topping (id, name, description, active, price) VALUES (2, 'Tomato', 'Red and nice addition', 1, 1.50);
+insert into Topping (id, name, description, active, price) VALUES (3, 'Peppers', 'Good for you', 1, 1.50);
+insert into Topping (id, name, description, active, price) VALUES (4, 'Onion', 'More flavour', 1, 1.50);
+insert into Topping (id, name, description, active, price) VALUES (5, 'Mushrooms', 'More flavour', 1, 1.50);
+insert into Topping (id, name, description, active, price) VALUES (6, 'Sweetcorn', 'More flavour', 1, 1.50);
+insert into Topping (id, name, description, active, price) VALUES (7, 'Chicken', 'Get more meat!', 1, 2.50);
+insert into Topping (id, name, description, active, price) VALUES (8, 'Pepperoni', 'Get more meat!', 1, 2.50);
 
